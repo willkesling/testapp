8c907ac258897c9afe0ecd80808b502caa0a86fe
Will Kesling <willkesling@Will-Kesling.leandog.net>
2016-05-31 15:17:42 -0400

    got static pages up and properly routed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,23 @@
 class StaticPagesController < ApplicationController
 
 	def show
+		if valid_page?
 		render template: "static_pages/#{params[:page]}"
+	else
+		render file: "public/404.html", status: :not_found
+	end
 	end
 
   def home
   end
 
   def help
+  end
+
+
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/static_pages/#{params[:pages]}.html.erb"))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  get "/static_pages/:page" => "static_pages#show"
+  get  'static_pages/test'
+
+  get "/static_pages/page" => "static_pages#show"
+  root "static_pages#show", page: "home"
+
+
+
+
 end
